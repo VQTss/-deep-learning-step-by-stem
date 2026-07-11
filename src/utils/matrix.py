eec24780fc96cdcd6e7470eca478e81d4830c280
thaivo <thaivo.software@gmail.com>
2026-07-11 18:34:23 +0900

    Update .gitignore to exclude __pycache__ directory and its contents

---
+++ src/utils/matrix.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def matrix_transpose(A):
+    """
+    Returns: ndarray, the transpose of A.
+    """
+    A = np.asarray(A,dtype=float)
+    return A.T
+
+
+def matix_trace(A):
+    """
+    Returns: float, the trace (sum of diagonal elements) of A.
+    """
+    A = np.asarray(A,dtype=float)
+    return np.trace(A)
+
+
+def matrix_vector_mutiply(A,x):
+    """
+    Returns: 1-D float64 array, the product A @ x.
+    """
+    A = np.asarray(A,dtype=float)
+    x = np.asarray(x,dtype=float)
+    return A @ x
+
+def hadamard_product(A,B):
+    """
+    Returns: ndarray, the element-wise product A * B.
+    """
+    A = np.asarray(A,dtype=float)
+    B = np.asarray(B,dtype=float)
+    return np.multiply(A,B)
